d7f533c55d9c2a2ab66e9df17bc430180f4ef691
Vpollo <vpollolee@gmail.com>
2020-02-14 17:22:46 -0500

    Make a small change for the test to reflect reality

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ print("Magic!")
 
 def main():
     for i in range(100):
-        print("I am shit")
+        print("Chiver is shit")
     a = input("input your goddamn name: ")
     print(f"Goodbye, {a}")
 
